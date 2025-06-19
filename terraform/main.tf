4f23215e2cdbda5a66efedcf177a876abaff4cce
Charlie <ctbushman@gmail.com>
2025-06-19 13:18:15 -0400

    Add web interface and status API

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,59 @@ resource "aws_s3_bucket" "backup" {
   bucket = var.backup_bucket_name
 }
 
+resource "aws_s3_bucket" "web" {
+  bucket = var.web_bucket_name
+  website {
+    index_document = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "web" {
+  bucket = aws_s3_bucket.web.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = "*",
+      Action    = ["s3:GetObject"],
+      Resource  = "${aws_s3_bucket.web.arn}/*"
+    }]
+  })
+}
+
+resource "aws_cloudfront_distribution" "web" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.web.bucket_regional_domain_name
+    origin_id   = "s3-web"
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-web"
+    viewer_protocol_policy = "redirect-to-https"
+    allowed_methods        = ["GET", "HEAD", "OPTIONS"]
+    cached_methods         = ["GET", "HEAD", "OPTIONS"]
+
+    forwarded_values {
+      query_string = false
+      cookies { forward = "none" }
+    }
+  }
+
+  price_class = "PriceClass_100"
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+}
+
 resource "aws_s3_bucket_lifecycle_configuration" "backup" {
   bucket = aws_s3_bucket.backup.id
 
@@ -124,7 +177,7 @@ resource "aws_eip_association" "minecraft" {
 }
 
 resource "aws_iam_role" "lambda" {
-  name = "start-minecraft-lambda-role"
+  name = "minecraft-lambda-role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
@@ -135,12 +188,12 @@ resource "aws_iam_role" "lambda" {
   })
 
   inline_policy {
-    name = "start-instance"
+    name = "minecraft-control"
     policy = jsonencode({
-      Version = "2012-10-17"
+      Version = "2012-10-17",
       Statement = [{
-        Effect   = "Allow"
-        Action   = ["ec2:StartInstances"]
+        Effect   = "Allow",
+        Action   = ["ec2:StartInstances", "ec2:DescribeInstances"],
         Resource = aws_instance.minecraft.arn
       }]
     })
@@ -148,8 +201,8 @@ resource "aws_iam_role" "lambda" {
 }
 
 resource "aws_lambda_function" "start_minecraft" {
-  filename         = data.archive_file.lambda.output_path
-  source_code_hash = data.archive_file.lambda.output_base64sha256
+  filename         = data.archive_file.lambda_start.output_path
+  source_code_hash = data.archive_file.lambda_start.output_base64sha256
   function_name    = "start-minecraft"
   role             = aws_iam_role.lambda.arn
   handler          = "start_minecraft.handler"
@@ -163,10 +216,33 @@ resource "aws_lambda_function" "start_minecraft" {
   }
 }
 
-data "archive_file" "lambda" {
+resource "aws_lambda_function" "status_minecraft" {
+  filename         = data.archive_file.lambda_status.output_path
+  source_code_hash = data.archive_file.lambda_status.output_base64sha256
+  function_name    = "status-minecraft"
+  role             = aws_iam_role.lambda.arn
+  handler          = "status_minecraft.handler"
+  runtime          = "python3.11"
+  timeout          = 10
+
+  environment {
+    variables = {
+      INSTANCE_ID = aws_instance.minecraft.id
+      SERVER_IP   = aws_eip.minecraft.public_ip
+    }
+  }
+}
+
+data "archive_file" "lambda_status" {
+  type        = "zip"
+  source_file = "${path.module}/lambda/status_minecraft.py"
+  output_path = "${path.module}/lambda_status.zip"
+}
+
+data "archive_file" "lambda_start" {
   type        = "zip"
   source_file = "${path.module}/lambda/start_minecraft.py"
-  output_path = "${path.module}/lambda.zip"
+  output_path = "${path.module}/lambda_start.zip"
 }
 
 resource "aws_lambda_permission" "apigw" {
@@ -177,8 +253,20 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = aws_api_gateway_rest_api.start.execution_arn
 }
 
+resource "aws_lambda_permission" "apigw_status" {
+  statement_id  = "AllowStatusExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.status_minecraft.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = aws_api_gateway_rest_api.status.execution_arn
+}
+
 resource "aws_api_gateway_rest_api" "start" {
   name = "start-minecraft-api"
+}
+
+resource "aws_api_gateway_rest_api" "status" {
+  name = "status-minecraft-api"
 }
 
 resource "aws_api_gateway_resource" "start" {
@@ -187,10 +275,23 @@ resource "aws_api_gateway_resource" "start" {
   path_part   = "start"
 }
 
+resource "aws_api_gateway_resource" "status" {
+  rest_api_id = aws_api_gateway_rest_api.status.id
+  parent_id   = aws_api_gateway_rest_api.status.root_resource_id
+  path_part   = "status"
+}
+
 resource "aws_api_gateway_method" "start" {
   rest_api_id   = aws_api_gateway_rest_api.start.id
   resource_id   = aws_api_gateway_resource.start.id
   http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_method" "status" {
+  rest_api_id   = aws_api_gateway_rest_api.status.id
+  resource_id   = aws_api_gateway_resource.status.id
+  http_method   = "GET"
   authorization = "NONE"
 }
 
@@ -203,9 +304,24 @@ resource "aws_api_gateway_integration" "start" {
   uri                     = aws_lambda_function.start_minecraft.invoke_arn
 }
 
+resource "aws_api_gateway_integration" "status" {
+  rest_api_id             = aws_api_gateway_rest_api.status.id
+  resource_id             = aws_api_gateway_resource.status.id
+  http_method             = aws_api_gateway_method.status.http_method
+  integration_http_method = "GET"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.status_minecraft.invoke_arn
+}
+
 resource "aws_api_gateway_deployment" "start" {
   depends_on  = [aws_api_gateway_integration.start]
   rest_api_id = aws_api_gateway_rest_api.start.id
+  stage_name  = "prod"
+}
+
+resource "aws_api_gateway_deployment" "status" {
+  depends_on  = [aws_api_gateway_integration.status]
+  rest_api_id = aws_api_gateway_rest_api.status.id
   stage_name  = "prod"
 }
 
@@ -219,4 +335,12 @@ output "backup_bucket" {
 
 output "start_minecraft_api_url" {
   value = aws_api_gateway_deployment.start.invoke_url
+}
+
+output "status_minecraft_api_url" {
+  value = aws_api_gateway_deployment.status.invoke_url
+}
+
+output "web_url" {
+  value = aws_cloudfront_distribution.web.domain_name
 }
