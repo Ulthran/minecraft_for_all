fdad4b0dbdd91fedc5554f47af9e697b7cd5d169
Charlie <ctbushman@gmail.com>
2025-06-25 00:47:58 -0400

    Enable SRP auth for user pool client

--- saas/modules/auth/main.tf
+++ saas/modules/auth/main.tf
@@ -42,9 +42,13 @@ EOF
 }
 
 resource "aws_cognito_user_pool_client" "this" {
-  name                = var.client_name
-  user_pool_id        = aws_cognito_user_pool.this.id
-  explicit_auth_flows = ["ALLOW_USER_PASSWORD_AUTH", "ALLOW_REFRESH_TOKEN_AUTH"]
+  name         = var.client_name
+  user_pool_id = aws_cognito_user_pool.this.id
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH",
+    "ALLOW_USER_SRP_AUTH",
+  ]
 }
 
 
