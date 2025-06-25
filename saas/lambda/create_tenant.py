2f365497c54961cd43d30295e7ae48e06f0159ac
Charlie <ctbushman@gmail.com>
2025-06-24 23:23:17 -0400

    Remove tenant attribute update and adjust docs

--- saas/lambda/create_tenant.py
+++ saas/lambda/create_tenant.py
@@ -1,12 +1,9 @@
-import boto3
 import logging
 import uuid
 from datetime import datetime
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
-
-cognito = boto3.client("cognito-idp")
 
 
 
@@ -19,19 +16,6 @@ def handler(event, context):
         logger.error("Email not found in event")
         return event
 
-    try:
-        user_pool_id = event["userPoolId"]
-    except KeyError:
-        logger.error("userPoolId not found in event")
-        return event
-
-    try:
-        username = event["userName"]
-    except KeyError:
-        logger.error("userName not found in event")
-        return event
-
-
     tenant_id = f"{uuid.uuid4().hex[:8]}-{datetime.utcnow().strftime('%y%m%d%H%M')}"
     logger.info("Assigned tenant id %s to %s", tenant_id, email)
 
@@ -39,19 +23,6 @@ def handler(event, context):
         "tenant_id": tenant_id,
     }
 
-    # Persist the new tenant ID
-    try:
-        cognito.admin_update_user_attributes(
-            UserPoolId=user_pool_id,
-            Username=username,
-            UserAttributes=[
-                {"Name": "custom:tenant_id", "Value": tenant_id},
-            ],
-        )
-        logger.info("Stored tenant id for %s", username)
-    except Exception:
-        logger.exception("Failed to set default attributes for %s", username)
-        return { "statusCode": 500, "body": "Failed to set default attributes" }
     
     logger.debug("Returning event: %s", event)
 
