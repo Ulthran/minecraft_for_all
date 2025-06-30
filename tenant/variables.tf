50b179bafc3bb77918aa518cd674283aed0b48da
Charlie <ctbushman@gmail.com>
2025-06-30 12:32:07 -0400

    Use default VPC and generate key pairs

--- tenant/variables.tf
+++ tenant/variables.tf
@@ -3,10 +3,6 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "key_pair_name" {
-  description = "Name of an existing EC2 key pair"
-  type        = string
-}
 
 variable "backup_bucket_name" {
   description = "Unique S3 bucket name for world backups"
@@ -14,15 +10,6 @@ variable "backup_bucket_name" {
   default     = "minecraft-saas-backups"
 }
 
-variable "vpc_id" {
-  description = "VPC for the security group"
-  type        = string
-}
-
-variable "subnet_id" {
-  description = "Subnet for the EC2 instance"
-  type        = string
-}
 
 
 variable "tenant_id" {
