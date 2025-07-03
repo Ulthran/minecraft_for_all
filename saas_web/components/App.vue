f6747b92bfa3fd5a906f9a4b8dd17d03b4ba3df6
Charlie <ctbushman@gmail.com>
2025-07-03 16:12:55 -0400

    Add responsive dropdown nav

--- saas_web/components/App.vue
+++ saas_web/components/App.vue
@@ -5,14 +5,33 @@
         <BlockBuddy sheet="iron" :index="0" :size="48" />
       </v-btn>
       <v-spacer></v-spacer>
-      <v-btn to="/pricing" variant="text" router>Pricing</v-btn>
-      <v-btn to="/support" variant="text" router>Support</v-btn>
-      <v-btn to="/privacy" variant="text" router>Privacy</v-btn>
-      <v-btn to="/about" variant="text" router>About Us</v-btn>
-      <v-btn v-if="!loggedIn" to="/login" variant="text" router>Login</v-btn>
-      <v-btn v-if="loggedIn" @click="logout" variant="text">Logout</v-btn>
-      <v-btn v-if="loggedIn" to="/console" variant="text" router>Console</v-btn>
-      <v-btn to="/start" color="primary" router>Start</v-btn>
+      <div class="d-none d-md-flex align-center">
+        <v-btn to="/pricing" variant="text" router>Pricing</v-btn>
+        <v-btn to="/support" variant="text" router>Support</v-btn>
+        <v-btn to="/privacy" variant="text" router>Privacy</v-btn>
+        <v-btn to="/about" variant="text" router>About Us</v-btn>
+        <v-btn v-if="!loggedIn" to="/login" variant="text" router>Login</v-btn>
+        <v-btn v-if="loggedIn" @click="logout" variant="text">Logout</v-btn>
+        <v-btn v-if="loggedIn" to="/console" variant="text" router>Console</v-btn>
+        <v-btn to="/start" color="primary" router>Start</v-btn>
+      </div>
+      <v-menu class="d-md-none" location="bottom end">
+        <template #activator="{ props }">
+          <v-btn icon v-bind="props">
+            <i class="fas fa-bars"></i>
+          </v-btn>
+        </template>
+        <v-list>
+          <v-list-item to="/pricing" link><v-list-item-title>Pricing</v-list-item-title></v-list-item>
+          <v-list-item to="/support" link><v-list-item-title>Support</v-list-item-title></v-list-item>
+          <v-list-item to="/privacy" link><v-list-item-title>Privacy</v-list-item-title></v-list-item>
+          <v-list-item to="/about" link><v-list-item-title>About Us</v-list-item-title></v-list-item>
+          <v-list-item v-if="!loggedIn" to="/login" link><v-list-item-title>Login</v-list-item-title></v-list-item>
+          <v-list-item v-if="loggedIn" @click="logout"><v-list-item-title>Logout</v-list-item-title></v-list-item>
+          <v-list-item v-if="loggedIn" to="/console" link><v-list-item-title>Console</v-list-item-title></v-list-item>
+          <v-list-item to="/start" link><v-list-item-title>Start</v-list-item-title></v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-main class="pa-15">
       <router-view></router-view>
