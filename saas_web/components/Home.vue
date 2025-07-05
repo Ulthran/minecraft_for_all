83097db79e7f81eb4370875d6dc43f5b750f17bc
Charlie <ctbushman@gmail.com>
2025-07-04 20:06:00 -0400

    Add speech bubble support

--- saas_web/components/Home.vue
+++ saas_web/components/Home.vue
@@ -4,50 +4,78 @@
       <!-- Segment 1 -->
       <v-row class="py-16 segment" align="center">
         <v-col cols="12" md="6">
-          <h2 class="text-h4 mb-4">The ONLY pure usage-based pricing available</h2>
-          <p>We are the only Minecraft server hosting service that has NO FLAT MONTHLY RATES. If you don't use our services for the month, you'll pay $0!</p>
+          <h2 class="text-h4 mb-4">
+            The ONLY pure usage-based pricing available
+          </h2>
+          <p>
+            We are the only Minecraft server hosting service that has NO FLAT
+            MONTHLY RATES. If you don't use our services for the month, you'll
+            pay $0!
+          </p>
         </v-col>
         <v-col cols="12" md="6" class="text-center">
-          <BlockBuddy sheet="iron" :index="0" :size="192" />
+          <BlockBuddy
+            sheet="iron"
+            :index="0"
+            :size="192"
+            message="No monthly rates!"
+          />
         </v-col>
       </v-row>
 
       <!-- Segment 2 -->
       <v-row class="py-16 segment" align="center">
         <v-col cols="12" md="6" class="text-center order-md-first order-last">
-          <BlockBuddy sheet="emerald" :index="1" :size="192" />
+          <BlockBuddy
+            sheet="emerald"
+            :index="1"
+            :size="192"
+            message="Transparent usage!"
+          />
         </v-col>
         <v-col cols="12" md="6">
           <h2 class="text-h4 mb-4">Transparent pricing</h2>
-          <p>You can track your usage of <i>compute</i>, <i>storage</i>, and <i>network bandwidth</i> in the Console.</p>
+          <p>
+            You can track your usage of <i>compute</i>, <i>storage</i>, and
+            <i>network bandwidth</i> in the Console.
+          </p>
         </v-col>
       </v-row>
 
       <!-- Segment 3 -->
       <v-row class="py-16 segment" align="center" justify="center">
         <v-col cols="12" class="text-center">
-          <v-btn color="primary" size="x-large" to="/start">
-            Start
-          </v-btn>
+          <v-btn color="primary" size="x-large" to="/start"> Start </v-btn>
         </v-col>
       </v-row>
 
       <!-- Segment 4 -->
       <v-row class="py-16 segment" align="center">
         <v-col cols="12" md="6" class="text-center">
-          <BlockBuddy sheet="diamond" :index="2" :size="192" />
+          <BlockBuddy
+            sheet="diamond"
+            :index="2"
+            :size="192"
+            message="Control your server!"
+          />
         </v-col>
         <v-col cols="12" md="6">
           <h2 class="text-h4 mb-4">Complete control</h2>
-          <p>You are given complete control over the </p>
+          <p>You are given complete control over the</p>
         </v-col>
       </v-row>
 
       <!-- Segment 5 -->
       <v-row class="py-16 segment" align="center">
         <v-col cols="12" md="6">
-          <h2 class="text-h4 mb-4">Enterprise-level features for peace of mind</h2>
-          <p>We provide all the "enterprise-level" features you'll see from other hosters such as automatic backups, DDoS protection, and more. Plus we've decoded what they even mean:</p>
+          <h2 class="text-h4 mb-4">
+            Enterprise-level features for peace of mind
+          </h2>
+          <p>
+            We provide all the "enterprise-level" features you'll see from other
+            hosters such as automatic backups, DDoS protection, and more. Plus
+            we've decoded what they even mean:
+          </p>
         </v-col>
         <v-col cols="12" md="6" class="text-center">
           <v-list class="text-left">
@@ -55,7 +83,11 @@
               <v-list-item-content>
                 <div>
                   <h3>Automatic backups</h3>
-                  <p>We automatically backup your server to our storage with industry leading availability and durability (99.999999999%).</p>
+                  <p>
+                    We automatically backup your server to our storage with
+                    industry leading availability and durability
+                    (99.999999999%).
+                  </p>
                 </div>
               </v-list-item-content>
             </v-list-item>
@@ -63,7 +95,10 @@
               <v-list-item-content>
                 <div>
                   <h3>DDoS protection</h3>
-                  <p>Protection against a wide range of known attacks is standard with every server you can use with us.</p>
+                  <p>
+                    Protection against a wide range of known attacks is standard
+                    with every server you can use with us.
+                  </p>
                 </div>
               </v-list-item-content>
             </v-list-item>
@@ -71,7 +106,10 @@
               <v-list-item-content>
                 <div>
                   <h3>Resource analytics</h3>
-                  <p>Dive deep on your resource usage to optimize costs for your players. It's what we're built for!</p>
+                  <p>
+                    Dive deep on your resource usage to optimize costs for your
+                    players. It's what we're built for!
+                  </p>
                 </div>
               </v-list-item-content>
             </v-list-item>
@@ -84,10 +122,13 @@
 
 <script>
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     BlockBuddy: Vue.defineAsyncComponent(() =>
-      window['vue3-sfc-loader'].loadModule(`${window.componentsPath}/BlockBuddy.vue`, window.loaderOptions)
+      window["vue3-sfc-loader"].loadModule(
+        `${window.componentsPath}/BlockBuddy.vue`,
+        window.loaderOptions,
+      ),
     ),
   },
 };
