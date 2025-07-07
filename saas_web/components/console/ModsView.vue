90767c7bdc94a0abd437a4a214d08fd980299522
Charlie <ctbushman@gmail.com>
2025-07-07 12:32:18 -0400

    Add console menu and placeholders

---
+++ saas_web/components/console/ModsView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h3 class="text-h6 mb-2">Mods</h3>
+    <p>Manage supported and custom mods for your server here.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ModsView",
+};
+</script>
+
+<style scoped></style>
